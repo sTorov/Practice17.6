75df88430ccb90bf28e49b63636546b9e558adbf
sTorov <sotorov2011@yandex.ru>
2022-09-15 20:09:29 +0300

    Добавьте файлы проекта.

---
+++ Task/Account.cs
@@ -0,0 +1,20 @@
+﻿namespace Task
+{
+    public class Account
+    {
+        /// <summary>
+        /// Тип учетной записи
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Баланс учетной записи
+        /// </summary>
+        public double Balance { get; set; }
+
+        /// <summary>
+        /// Процентная ставка
+        /// </summary>
+        public double Interest { get; set; }
+    }
+}
---
+++ Task/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace Task
+{
+    public static class Calculator
+    {
+        /// <summary>
+        /// Метод для расчета процентной ставки
+        /// </summary>
+        /// <param name="account"></param>
+        public static void CalculateInterest(Account account)
+        {
+            if (account.Type == "Обычный")
+            {
+                // расчет процентной ставки обычного аккаунта по правилам банка
+                account.Interest = account.Balance * 0.4;
+
+                if (account.Balance < 1000)
+                    account.Interest -= account.Balance * 0.2;
+
+                if (account.Balance < 50000)
+                    account.Interest -= account.Balance * 0.4;
+            }
+            else if (account.Type == "Зарплатный")
+            {
+                // расчет процентной ставк зарплатного аккаунта по правилам банка
+                account.Interest = account.Balance * 0.5;
+            }
+        }
+    }
+}
